47ef76a20d0a8c2be4929668cc58293c548b2032
Harshit Soni <harshitsoni3003@gmail.com>
2026-03-03 16:48:15 +0530

    first commit

---
+++ uc3.java
@@ -0,0 +1,9 @@
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        System.out.println(String.join(" ", " OOOO ", " OOOO ", "PPPP  ", " SSSS "));
+        System.out.println(String.join(" ", "O    O", "O    O", "P   P ", "S     "));
+        System.out.println(String.join(" ", "O    O", "O    O", "PPPP  ", " SSSS "));
+        System.out.println(String.join(" ", "O    O", "O    O", "P     ", "    S "));
+        System.out.println(String.join(" ", " OOOO ", " OOOO ", "P     ", "SSSS  "));
+    }
+}
